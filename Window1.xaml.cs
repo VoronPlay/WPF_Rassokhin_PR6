9cabb0f6fcecd907addaa3293bd2bc30f02a7bdd
visual@ap47.ru <visual@ap47.ru>
2022-10-05 10:39:12 +1000

    полировка проекта

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -27,15 +27,12 @@ namespace WPF_Rassokhin_PR6
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             {
-                // This block sets the TextBlock to a sensible default if dates haven't been picked
                 if (!datePicker1.SelectedDate.HasValue || !datePicker2.SelectedDate.HasValue)
                 {
                     textBlock1.Text = "Select dates";
                     return;
                 }
 
-                // Because the nullable SelectedDate properties must have a value to reach this point, 
-                // we can safely reference them - otherwise, these statements throw, as you've discovered.
                 DateTime start = datePicker1.SelectedDate.Value.Date;
                 DateTime finish = datePicker2.SelectedDate.Value.Date;
                 TimeSpan difference = finish.Subtract(start);
